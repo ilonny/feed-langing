2bb109f26f5595e0302234797fe1fac235a1f0ba
ilonny <lonnyfox@bk.ru>
2021-03-31 00:43:20 +0300

    second screen start animate

--- script.js
+++ script.js
@@ -12,8 +12,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("down-icon").addEventListener("click", () => {
         console.log("clicked");
         window.scrollTo({
-            top: document.getElementById("section-2").offsetTop - 120,
-            behavior: "smooth"
+            top: document.getElementById("section-2").offsetTop - 280,
+            behavior: "smooth",
         });
+    });
+    window.addEventListener("scroll", (e) => {
+        // console.log("scroll", window.scrollY);
+        const { scrollY } = window;
+        if (scrollY > 810) {
+            //animate second sections
+            let animElements = document.querySelectorAll(
+                ".opacity-translate-right"
+            );
+            animElements.forEach((el) => {
+                let forTimeout = Number(el.dataset.delay);
+                setTimeout(() => {
+                    el.classList.remove("opacity-translate-right");
+                }, forTimeout);
+            });
+        }
     });
 });
