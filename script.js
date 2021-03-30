b999e7e6d73c3ed36637e5d4c520dd08ddab9663
ilonny <lonnyfox@bk.ru>
2021-03-30 23:47:02 +0300

    first-section

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    //animate first screen elements
+    let firstAnimElements = document.querySelectorAll(".transition-animate");
+    firstAnimElements.forEach((el) => {
+        let forTimeout = Number(el.dataset.delay);
+        setTimeout(() => {
+            el.classList.remove("opacity-translate-hidden");
+        }, forTimeout);
+    });
+    //click on down button - scroll to second Sections
+    document.getElementById("down-icon").addEventListener("click", () => {
+        console.log("clicked");
+        window.scrollTo({
+            top: document.getElementById("section-2").offsetTop - 120,
+            behavior: "smooth"
+        });
+    });
+});
